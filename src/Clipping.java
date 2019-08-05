cdeaf0f291327d51d195bf200fdb233421fcc426
WilliamCadenas21 <williamcadenas21@gmail.com>
2019-08-04 20:23:31 -0500

    project 50% completed

--- src/Clipping.java
+++ src/Clipping.java
@@ -1,3 +1,4 @@
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -9,10 +10,16 @@ import javax.swing.JPanel;
 import javax.swing.JFrame;
 
 public class Clipping extends JPanel {
-    
-    
-    public int numberOfPoints = 6;
-    
+
+    private final int numberOfPoints = 6;
+    private final double angulo = 10;
+
+    private static final int INSIDE = 0; //0000
+    private static final int LEFT = 1; //0001
+    private static final int RIGHT = 2; //0010
+    private static final int BOTTOM = 4; //0100
+    private static final int TOP = 8; //1000
+
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -28,7 +35,10 @@ public class Clipping extends JPanel {
 
         int w = size.width - insets.left - insets.right;
         int h = size.height - insets.top - insets.bottom;
-
+        
+        g2d.drawLine(0, h / 2, w, h / 2);// eje x
+        g2d.drawLine(w / 2, 0, w / 2, h);// eje y
+        
         int x = 100;
         int y = 100;
 
@@ -41,27 +51,31 @@ public class Clipping extends JPanel {
         for (int i = 0; i < 4; i++) {
             drawJava(boundary[i].x, boundary[i].y, boundary[(i + 1) % 4].x, boundary[(i + 1) % 4].y, g2d, w, h); //eje x
         }
-        
+
         Point[] points = new Point[numberOfPoints];
+        //one line
         points[0] = new Point(-150, -150);
         points[1] = new Point(50, 50);
+        //second line
         points[2] = new Point(-50, 150);
         points[3] = new Point(50, 150);
+        //tree line
         points[4] = new Point(110, 50);
         points[5] = new Point(160, 80);
 
         CohenSutherLand(points, boundary, g2d, w, h);
     }
-    
+
     /**
-     * this is a method created for help to draw lines in Cartesian coordinates 
+     * this is a method created for help to draw lines in Cartesian coordinates
+     *
      * @param x1
      * @param y1
      * @param x2
      * @param y2
      * @param g2d
      * @param w
-     * @param h 
+     * @param h
      */
     public static void drawJava(int x1, int y1, int x2, int y2, Graphics2D g2d, int w, int h) {
         int xj1 = w / 2 + x1;
@@ -85,51 +99,100 @@ public class Clipping extends JPanel {
         // Mostrar el frame
         frame.setVisible(true);
     }
+
     /**
-     * this is the main method to calculate if a line or segment is 
-     * inside or outside of a clipping area
+     * this is the main method to calculate if a line or segment is inside or
+     * outside of a clipping area
+     *
      * @param points the points that make the segments
      * @param boundary the array that contains the limits points
-     * @param g2d 
+     * @param g2d
      * @param w the weight of the window
-     * @param h the high of the window 
+     * @param h the high of the window
      */
-    private void CohenSutherLand(Point[] points, Point[] boundary,Graphics2D g2d, int w, int h) {
-        for (int i = 0; i < points.length; i=i+2) {
-            
+    private void CohenSutherLand(Point[] points, Point[] boundary, Graphics2D g2d, int w, int h) {
+        boolean extraSegment = false;
+        for (int i = 0; i < points.length; i = i + 2) {
+
             int startCode = getCode(points[i], boundary);
-            int endCode = getCode(points[i+1], boundary);
-            
-            System.out.println("s:"+startCode+"e:"+endCode);
-            if ((startCode | endCode) == 0) {
-                g2d.setColor(Color.blue);// se encuentra dentro
-                System.out.println("Adentro");
-            }else if((startCode & endCode) !=0){
-                g2d.setColor(Color.red);//a fuera
-                System.out.println("afuera");
-            }else{
-                g2d.setColor(Color.green);//a fuera
-                System.out.println("No se puede deteminar por las dos pruebas de trivialidad");
+            int endCode = getCode(points[i + 1], boundary);
+            Point pi = new Point(points[i].x, points[i].y);
+            Point pf = new Point(points[i + 1].x, points[i + 1].y);
+
+            while (true) {
+                if ((startCode | endCode) == 0) {
+                    System.out.println("Adentro");
+                    break;
+                } else if ((startCode & endCode) != 0) {
+                    System.out.println("afuera");
+                    break;
+                } else {
+                    System.out.println("Crea un nuevo segmento");
+                    int x = 0, y = 0;
+                    extraSegment = true;
+
+                    int x0 = pi.x;
+                    int y0 = pi.y;
+                    int x1 = pf.x;
+                    int y1 = pf.y;
+                    int ymax = boundary[2].y;
+                    int ymin = boundary[0].y;
+                    int xmax = boundary[2].x;
+                    int xmin = boundary[0].x;
+
+                    int outCode = (startCode != 0) ? startCode : endCode;
+
+                    if ((outCode & TOP) != 0) {
+                        x = x0 + (x1 - x0) * (ymax - y0) / (y1 - y0);
+                        y = ymax;
+                    } else if ((outCode & BOTTOM) !=  0) {
+                        x = x0 + (x1 - x0) * (ymin - y0) / (y1 - y0);
+                        y = ymin;
+                    } else if ((outCode & RIGHT) != 0) {
+                        y = y0 + (y1 - y0) * (xmax - x0) / (x1 - x0);
+                        x = xmax;
+                    } else if ((outCode & LEFT) != 0) {
+                        y = y0 + (y1 - y0) * (xmin - x0) / (x1 - x0);
+                        x = xmin;
+                    }
+
+                    if (outCode == startCode) {
+                        pi.x = x;
+                        pi.y = y;
+                        startCode = getCode(pi, boundary);
+                    } else {
+                        pf.x = x;
+                        pf.y = y;
+                        endCode = getCode(pf, boundary);
+                    }
+                }
             }
             
-            drawJava(points[i].x, points[i].y, points[i+1].x, points[i+1].y, g2d, w, h);
+            g2d.setColor(Color.red);
+            drawJava(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y, g2d, w, h);
+
+            if (extraSegment) {
+                g2d.setColor(Color.green);// se encuentra dentro
+                drawJava(pi.x, pi.y, pf.x, pf.y, g2d, w, h);
+                extraSegment = false;
+            }
         }
     }
-    
+
     //funcion necesaria para la implementacion de CohenSutherland
     private int getCode(Point points, Point[] boundary) {
-        int result = 0; // Inside 0000
+        int result = INSIDE; // 0000
 
         if (points.x < boundary[0].x) {
-            result = 1; // Left 0001
+            result = LEFT; // 0001
         } else if (points.x > boundary[2].x) {
-            result = 2; // Right 0010
+            result = RIGHT; // 0010
         }
 
         if (points.y < boundary[0].y) {
-            result = 4; // Bottom 0100
+            result = BOTTOM; // 0100
         } else if (points.y > boundary[2].y) {
-            result = 8; // Top
+            result = TOP; // 1000
         }
         return result;
     }
